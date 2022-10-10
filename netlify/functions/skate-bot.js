c0c4ad2c3c511901a0643582f907f03981933cbf
Samuli Välimäki <samuli.valimaki@sitowise.com>
2022-10-10 19:31:31 +0300

    test if working

--- netlify/functions/skate-bot.js
+++ netlify/functions/skate-bot.js
@@ -29,9 +29,9 @@ app.post("/", (req, res) => {
     });
 });
 // Listening
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
