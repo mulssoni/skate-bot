81ae760a02a6aea19006ca51896391b46f281d4c
Samuli Välimäki <samuli.valimaki@sitowise.com>
2022-10-10 15:51:11 +0300

    test netlify way

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ const serverless = require("serverless-http");
 // Configurations
 app.use(bodyParser.json());
 // Endpoints
-app.post("/", (req, res) => {
-  const chatId = req.body.message.chat.id;
-  const sentMessage = req.body.message.text;
+exports.handler = async function (event, context) {
+  app.post("/", (req, res) => {
+    const chatId = req.body.message.chat.id;
+    const sentMessage = req.body.message.text;
 
-  axios
-    .post(`${url}${apiToken}/sendMessage`, {
-      chat_id: chatId,
-      text: `hello back 👋 You just said: ${sentMessage}`,
-    })
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.send(error);
-    });
-});
-// Listening
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-module.exports.handler = serverless(app);
+    axios
+      .post(`${url}${apiToken}/sendMessage`, {
+        chat_id: chatId,
+        text: `hello back 👋 You just said: ${sentMessage}`,
+      })
+      .then((response) => {
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.send(error);
+      });
+  });
+  // Listening
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
