2f28f542979fe00f06b9d36bfbb793f71be004f2
Samuli Välimäki <samuli.valimaki@sitowise.com>
2022-10-12 22:54:44 +0300

    test scheduled functions

--- src/sendSkatePoll.js
+++ src/sendSkatePoll.js
@@ -1,22 +1,34 @@
 require("dotenv").config();
-const axios = require("axios");
 const sendPoll = require("./sendPoll");
-const url = "https://api.telegram.org/bot";
-const { TELEGRAM_ACCESS_TOKEN } = process.env;
+const isTuesday = require("date-fns/isTuesday");
+const isThursday = require("date-fns/isThursday");
 const nextTuesday = require("date-fns/nextTuesday");
-const nextTuesday = require("date-fns/nextTuesday");
-const differenceInCalendarDays = require("date-fns/differenceInCalendarDays");
+const nextThursday = require("date-fns/nextThursday");
+const format = require("date-fns/format");
+// const differenceInCalendarDays = require("date-fns/differenceInCalendarDays");
 const add = require("date-fns/add");
 
 // DOES NOT WORK LIKE THIS
-const targetPollDateTuesday = differenceInCalendarDays(
-  nextTuesday(add(new Date(), { days: 7 })),
-  new Date()
-);
+const targetPollDateTuesday = nextTuesday(add(new Date(), { weeks: 1 })); // 2 weeks
+const targetPollDateThursday = nextThursday(add(new Date(), { weeks: 1 })); // 2 weeks
+const today = new Date();
 
 module.exports = async () => {
-  await sendPoll(5616549051, `Osallistun ${targetPollDateTuesday} vuoroon!`, [
-    "Kyllä",
-    "Ei",
-  ]);
+  if (isTuesday(today)) {
+    await sendPoll(
+      5616549051,
+      `Osallistun ${format(targetPollDateTuesday, "ccc d.M")} vuoroon! 
+      Ilmoita viimeistään viikkoa aikaisemmin`,
+      ["Kyllä", "Ei"]
+    );
+  }
+
+  if (isThursday(today)) {
+    await sendPoll(
+      5616549051,
+      `Osallistun ${format(targetPollDateThursday, "ccc d.M")} vuoroon! 
+      Ilmoita viimeistään viikkoa aikaisemmin`,
+      ["Kyllä", "Ei"]
+    );
+  }
 };
