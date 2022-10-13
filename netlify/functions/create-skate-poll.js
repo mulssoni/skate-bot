a03c19eb5c7f7b8c9afe35994a5ec1096c8986e6
Samuli Välimäki <samuli.valimaki@sitowise.com>
2022-10-13 09:51:26 +0300

    Test scheduled function with message

--- netlify/functions/create-skate-poll.js
+++ netlify/functions/create-skate-poll.js
@@ -1,6 +1,9 @@
+const sendMessage = require("../../src/sendMessage");
 const sendSkatePoll = require("../../src/sendSkatePoll");
 
 exports.handler = async () => {
   await sendSkatePoll();
+  await sendMessage(5616549051, "Scheduled function working");
+
   return { statusCode: 200 };
 };
