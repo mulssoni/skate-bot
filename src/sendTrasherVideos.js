2cd84012514df46649f2169b3bfc67a5205544e9
Samuli Välimäki <samuli.valimaki@sitowise.com>
2022-10-20 16:34:18 +0300

    try array join

--- src/sendTrasherVideos.js
+++ src/sendTrasherVideos.js
@@ -5,11 +5,9 @@ module.exports = async () => {
     const res = await axios.get(
       "https://skate-api.netlify.app/.netlify/functions/trasher"
     );
-    let arr = [];
     const str =
-      res.data?.map((item) => arr.push(`${item.title}\n${item.link}`)) ||
-      "No videos!";
-    return arr.join("\n\n");
+      res.data?.map((item) => `${item.title}\n${item.link}`) || "No videos!";
+    return str.join("\n\n");
   } catch {
     return "Couldn't get new videos";
   }
